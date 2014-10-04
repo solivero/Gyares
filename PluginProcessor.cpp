40a3c5af5872a474791f5fa2c0b4933aa021cbf4
Oliver Petri <oliver.ce.petri@gmail.com>
2014-10-05 00:47:48 +0200

    Testing

--- PluginProcessor.cpp
+++ PluginProcessor.cpp
@@ -10,11 +10,146 @@
 
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
+ //==============================================================================
+/** A demo synth sound that's just a basic sine wave.. */
+class SineWaveSound : public SynthesiserSound
+{
+public:
+    SineWaveSound() {}
 
+    bool appliesToNote (int /*midiNoteNumber*/) override  { return true; }
+    bool appliesToChannel (int /*midiChannel*/) override  { return true; }
+};
+
+//==============================================================================
+/** A simple demo synth voice that just plays a sine wave.. */
+class SineWaveVoice  : public SynthesiserVoice
+{
+public:
+    SineWaveVoice()
+        : angleDelta (0.0),
+          tailOff (0.0)
+    {
+    }
+
+    bool canPlaySound (SynthesiserSound* sound) override
+    {
+        return dynamic_cast<SineWaveSound*> (sound) != nullptr;
+    }
+
+    void startNote (int midiNoteNumber, float velocity,
+                    SynthesiserSound* /*sound*/,
+                    int /*currentPitchWheelPosition*/) override
+    {
+        currentAngle = 0.0;
+        level = velocity * 0.15;
+        tailOff = 0.0;
+
+        double cyclesPerSecond = MidiMessage::getMidiNoteInHertz (midiNoteNumber);
+        double cyclesPerSample = cyclesPerSecond / getSampleRate();
+
+        angleDelta = cyclesPerSample * 2.0 * double_Pi;
+    }
+
+    void stopNote (float velocity, bool allowTailOff) override
+    {
+        if (allowTailOff)
+        {
+            // start a tail-off by setting this flag. The render callback will pick up on
+            // this and do a fade out, calling clearCurrentNote() when it's finished.
+
+            if (tailOff == 0.0) // we only need to begin a tail-off if it's not already doing so - the
+                                // stopNote method could be called more than once.
+                tailOff = 1.0;
+        }
+        else
+        {
+            // we're being told to stop playing immediately, so reset everything..
+
+            clearCurrentNote();
+            angleDelta = 0.0;
+        }
+    }
+
+    void pitchWheelMoved (int /*newValue*/) override
+    {
+        // can't be bothered implementing this for the demo!
+    }
+
+    void controllerMoved (int /*controllerNumber*/, int /*newValue*/) override
+    {
+        // not interested in controllers in this case.
+    }
+
+    void renderNextBlock (AudioSampleBuffer& outputBuffer, int startSample, int numSamples) override
+    {
+        if (angleDelta != 0.0)
+        {
+            if (tailOff > 0)
+            {
+                while (--numSamples >= 0)
+                {
+                    const float currentSample = (float) (sin (currentAngle) * level * tailOff);
+
+                    for (int i = outputBuffer.getNumChannels(); --i >= 0;)
+                        outputBuffer.addSample (i, startSample, currentSample);
+
+                    currentAngle += angleDelta;
+                    ++startSample;
+
+                    tailOff *= 0.99;
+
+                    if (tailOff <= 0.005)
+                    {
+                        clearCurrentNote();
+
+                        angleDelta = 0.0;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                while (--numSamples >= 0)
+                {
+                    const float currentSample = (float) (sin (currentAngle) * level);
+
+                    for (int i = outputBuffer.getNumChannels(); --i >= 0;)
+                        outputBuffer.addSample (i, startSample, currentSample);
+
+                    currentAngle += angleDelta;
+                    ++startSample;
+                }
+            }
+        }
+    }
+
+private:
+    double currentAngle, angleDelta, level, tailOff;
+};
+
+const float defaultGain = 1.0f;
+const float defaultDelay = 0.5f;
 
 //==============================================================================
 GyaresAudioProcessor::GyaresAudioProcessor()
+    : delayBuffer (2, 12000)
 {
+    // Set up some default values..
+    gain = defaultGain;
+    delay = defaultDelay;
+
+    lastUIWidth = 400;
+    lastUIHeight = 200;
+
+    lastPosInfo.resetToDefault();
+    delayPosition = 0;
+
+    // Initialise the synth...
+    for (int i = 4; --i >= 0;)
+        synth.addVoice (new SineWaveVoice());   // These voices will play our custom sine-wave sounds..
+
+    synth.addSound (new SineWaveSound());
 }
 
 GyaresAudioProcessor::~GyaresAudioProcessor()
